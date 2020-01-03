8b49da3b94562481abcdef418e09e7f2f167936a
corey gibson <cgibsonmm@gmail.com>
2020-01-03 17:34:17 -0500

    added tailwindd

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import { t } from  'react-native-tailwindcss'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>hello</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
